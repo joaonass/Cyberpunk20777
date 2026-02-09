35c311ed4df21e17d3c7c25745e8a033cf5d3115
joaonass <joaonass04@gmail.com>
2026-02-09 13:41:51 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let video = document.getElementById("video");
+
+if (sessionStorage.getItem("introPlayed")) {
+  video.remove();
+} else {
+  video.play();
+  sessionStorage.setItem("introPlayed", "true");
+
+  video.addEventListener("ended", function () {
+    video.remove();
+  });
+}
+
+
+
+
